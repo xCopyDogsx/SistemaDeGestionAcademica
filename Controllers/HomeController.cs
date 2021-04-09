fec51422f346342bc67d55e1b3cbe930806879de
Brian Cañón Rojas <41877674+xCopyDogsx@users.noreply.github.com>
2021-04-08 19:20:59 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoFinal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult SGA()
+        {
+            return View();
+        }
+        public ActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
