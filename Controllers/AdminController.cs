df81af9770a4a871a48e50cb9fd3623a4759011f
Brian Cañón Rojas <41877674+xCopyDogsx@users.noreply.github.com>
2021-04-23 15:56:43 -0500

    Se añaden funciones del login y sesiones

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoFinal.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["User"] == null)
+            {
+          
+                Response.Redirect("~/Home/");
+            }
+            if (Request.HttpMethod == "POST")
+            {
+                if (Session["User"] == null)
+                {
+                    Response.Redirect("~/Home/");
+                }
+            }
+        }
+
+        public ActionResult Estudiantes()
+        {
+            if (Session["User"] == null)
+            {
+               HomeController home = new HomeController();
+                ActionResult resultado = home.Index();
+                return resultado;
+            }else if(Session["Rol"].Equals("Adminstrador"))
+            {
+                return View();
+            }
+            else
+            {
+                HomeController home = new HomeController();
+                ActionResult resultado = home.Index();
+                return resultado;
+            }
+            
+        }
+        public ActionResult Docentes()
+        {
+            if (Session["User"] == null)
+            {
+                HomeController home = new HomeController();
+                return home.Index();
+            }
+            else if (Session["Rol"].Equals("Adminstrador"))
+            {
+                return View();
+            }
+            else
+            {
+                HomeController home = new HomeController();
+                return home.Index();
+            }
+        }
+        public ActionResult Periodos()
+        {
+            if (Session["User"] == null)
+            {
+                HomeController home = new HomeController();
+                return home.Index();
+            }
+            else if (Session["Rol"].Equals("Adminstrador"))
+            {
+                return View();
+            }
+            else
+            {
+                HomeController home = new HomeController();
+                return home.Index();
+            }
+        }
+       public ActionResult Materias()
+        {
+            if (Session["User"] == null)
+            {
+                HomeController home = new HomeController();
+                return home.Index();
+            }
+            else if (Session["Rol"].Equals("Adminstrador"))
+            {
+                return View();
+            }
+            else
+            {
+                HomeController home = new HomeController();
+                return home.Index();
+            }
+        }
+        public ActionResult Admins()
+        {
+            if (Session["User"] == null)
+            {
+                HomeController home = new HomeController();
+                return home.Index();
+            }
+            else if (Session["Rol"].Equals("Adminstrador"))
+            {
+                return View();
+            }
+            else
+            {
+                HomeController home = new HomeController();
+                return home.Index();
+            }
+        }
+
+    }
+}
--- Controllers/PanelController.cs
+++ Controllers/PanelController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace ProyectoFinal.Controllers
 {
@@ -12,6 +13,13 @@ namespace ProyectoFinal.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public void Salir()
+        {
+            FormsAuthentication.SignOut();
+            Session.RemoveAll();
+            Session.Abandon();
+            Response.Redirect("~/Home/");
         }
     }
 }
