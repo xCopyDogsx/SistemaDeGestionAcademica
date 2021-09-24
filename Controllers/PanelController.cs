24118f94a0d79a95ce7b3d797f7a15684530198a
Brian Cañon Rojas <canonbrian123@hotmail.com>
2021-09-24 14:10:18 -0500

    Graficas dashboard basicas

--- Controllers/PanelController.cs
+++ Controllers/PanelController.cs
@@ -14,6 +14,7 @@ namespace ProyectoFinal.Controllers
         public ActionResult Index()
         {
             ViewBag.Activar = "Panel";
+            List<periodo> lst = null;
             if (Session["User"] != null)
             {
                 if (Session["Rol"].Equals("Administrador"))
@@ -33,6 +34,22 @@ namespace ProyectoFinal.Controllers
                         ViewBag.Admin = Admin;
                         ViewBag.Padre = Padre;
                     }
+               
+                    using (sgaEntities bdx = new sgaEntities())
+                    {
+                        lst = (from per in bdx.periodo
+                               select per).ToList();
+                    }
+                    List<SelectListItem> items = lst.ConvertAll(d => {
+                        return new SelectListItem()
+                        {
+                            Text = d.Per_Nom,
+                            Value = d.Per_Nom,
+                            Selected = false
+                        };
+                    });
+
+                    ViewBag.Items = items;
                 }
             }
             return View();
@@ -56,6 +73,43 @@ namespace ProyectoFinal.Controllers
             Session.RemoveAll();
             Session.Abandon();
             return RedirectToAction("Index", "Home");
+        }
+        [HttpPost]
+        public ActionResult GeneraGraf(string period)
+        {
+            int activos=0, inactivos=0;
+            using (sgaEntities db = new sgaEntities())
+            {
+                var query = (from per in db.periodo
+                             where per.Per_Nom.Equals(period)
+                             select per).FirstOrDefault();
+                if (query == null) return Json(new { Success = false, msg = "Ese periodo no existe" });
+                activos += (from es in db.alumno
+                           where es.Alum_Status==1
+                           select es).Count();
+                activos += (from ad in db.administrador
+                            where ad.Adm_Status == 1
+                            select ad).Count();
+                activos += (from doc in db.docente
+                            where doc.Doc_Status == 1
+                            select doc).Count();
+                activos += (from acu in db.acudiente
+                            where acu.Acu_Status == 1
+                            select acu).Count();
+                inactivos += (from es in db.alumno
+                            where es.Alum_Status != 1
+                            select es).Count();
+                inactivos += (from ad in db.administrador
+                            where ad.Adm_Status != 1
+                            select ad).Count();
+                inactivos += (from doc in db.docente
+                            where doc.Doc_Status != 1
+                            select doc).Count();
+                inactivos += (from acu in db.acudiente
+                            where acu.Acu_Status != 1
+                            select acu).Count();
+            }
+            return Json(new { Success = true, activos, inactivos });
         }
     }
 }
